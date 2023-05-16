a800de92b2953a0ec48f9fab326c705e2eb3eb09
Hari-Krishnan-Geek <113627237+Hari-Krishnan-Geek@users.noreply.github.com>
2023-05-17 00:26:47 +0530

    Time: 43 ms (26.12%), Space: 16.5 MB (7.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        # Solving this problem using dynamic programming
-        # dp[i] stores the maximim amount that can be robbed till house 'i'
-
-        n = len(nums)
-        if n == 1:
-            # if only one house, the maximum amount robbed is the amount in that house alone.
+        # solving using memoization
+        if len(nums) == 1:
             return nums[0]
 
-        dp = [0 for _ in range(n)]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        memo = [nums[0], max(nums[0], nums[1])]
 
-        for i in range(2, n):
+        def rec(nums):
+            if nums:
+                memo.append(max(memo[-1], nums[0] + memo[-2]))
+                rec(nums[1:])
 
-            # The maximum amount thant can be robbed as a total is =
-            # max(maximum amount that can be robbed till 'i-1' th house, amount in current house +
-            # maximum amount that can be robbed till 'i-2' th house)
-            dp[i] = max(dp[i-1], (nums[i] + dp[i-2]))
+        rec(nums[2:])
 
-        # returns the maximum amount that can be robbed if we visit all the houses
-        return dp[-1]
+        return memo[-1]
+
