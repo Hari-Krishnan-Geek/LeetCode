b6924b3734074eec853e65a7834b97e47edd1774
Hari-Krishnan-Geek <113627237+Hari-Krishnan-Geek@users.noreply.github.com>
2023-08-18 23:42:50 +0530

    Time: 3366 ms (61.63%), Space: 16.7 MB (45.37%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,24 +1,12 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         
-        mem = [-1] * (n+1)
+        dp = [i for i in range(n+1)]
 
-        def solve(n):
-            if mem[n] != -1:
-                return mem[n]
-            if n == 0:
-                mem[0] = 0
-                return mem[0]
-
-            elem = n - 1
-            i = 1
-            while ((i*i) <= n):
-                elem = min(elem, solve(n - (i*i)))
-                i += 1
-  
-            mem[n] = elem + 1
-            return mem[n]
-
-        return solve(n)
-
+        for i in range(n+1):
+            j = 1
+            while j*j <= i:
+                dp[i] = min(dp[i], dp[i - (j * j)] + 1)
+                j += 1
+        return dp[n]
 
